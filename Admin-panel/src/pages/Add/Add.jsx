0a56cfcdb9c41d3ca50feecde972b7b0533ebfaa
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-12-12 21:07:41 +0530

    created onChangeHandler, onSubmitHandler and created formData

--- Admin-panel/src/pages/Add/Add.jsx
+++ Admin-panel/src/pages/Add/Add.jsx
@@ -1,29 +1,56 @@
 
+import { useState } from 'react';
 import './add.css'
 
 function Add() {
+
+    const [image, setImage] = useState(false);
+    const [data, setData] = useState({
+        name:"",
+        description:"",
+        price:"",
+        category: "Salad"
+    });
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data => ({...data, [name]: value}) )
+    }
+
+    // making api call
+    // the primary use FormData is native supports of file input as in (<input type="file" />), which is used to compile data in key value pair.
+    const onSubmitHandler = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append("name", data.name)
+        formData.append("description", data.description)
+        formData.append("price", data.price)
+        formData.append("category", data.category)
+    }
+
   return (
     <div className='add'>
-        <form className='flex-col'>
+        <form className='flex-col' onSubmit={onSubmitHandler}>
             <div className="add-image-upload ">
                 <p>Upload Image</p>
                 <label htmlFor="image">
-                    <p><span class=" upload material-symbols-outlined">upload</span></p>
+                    <p> {image? <img src={URL.createObjectURL(image)} alt="preview" /> : <><span className="upload material-symbols-outlined">upload</span> <span className='image-p'>images</span></>}   </p>
                 </label>
-                <input type="file" id='image' hidden required />
+                <input onChange={(e) => setImage(e.target.files[0])} type="file" id='image' hidden required />
             </div>
             <div className="add-product-name">
                 <p>Product name</p>
-                <input type="text" name='name' placeholder='Type here' />
+                <input onChange={onChangeHandler} value={data.name} type="text" name='name' placeholder='Type here' />
             </div>
             <div className='add-product-description flex-col'>
                 <p>Product description</p>
-                <textarea name="description" rows="6" placeholder='write content here' ></textarea>
+                <textarea onChange={onChangeHandler} value={data.description} name="description" rows="6" placeholder='write content here' ></textarea>
             </div>
             <div className="add-category-price">
                 <div className="add-category flex-col">
                     <p>Product Category</p>
-                    <select name="category">
+                    <select onChange={onChangeHandler} value={data.category} name="category">
                         <option value="Salad">Salad</option>
                         <option value="Rolls">Rolls</option>
                         <option value="Sandwich">Sandwich</option>
@@ -35,7 +62,7 @@ function Add() {
                 </div>
                 <div className="add-price flex-col">
                     <p>Product price</p>
-                    <input type="Number" name='price' placeholder='$20' />
+                    <input onChange={onChangeHandler} value={data.price} type="Number" name='price' placeholder='$20' />
                 </div>
             </div>
             <button type='submit' className='add-button'>ADD</button>
