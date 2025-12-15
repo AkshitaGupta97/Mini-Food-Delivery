a0e791690c865f6130a2d21ee999abf7b803a612
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-12-15 15:14:22 +0530

    added removeFromCart logic in cartController

--- backend-food/controllers/cartController.js
+++ backend-food/controllers/cartController.js
@@ -3,7 +3,7 @@ import userModel from "../models/userModels.js"
 // add items to user cart
 const addToCart = async (req, res) => {
     try {
-        let userData = await userModel.findOne({_id:req.body.userId});
+        let userData = await userModel.findOne(req.body.userId);
         let cartData = await userData.cartData;
         if(!cartData[req.body.itemId]){  // if the user want to add product in cart with itemId, and with that itemId there is no entry in cart, in that case it will create a new entry.
             cartData[req.body.itemId] = 1;
@@ -22,13 +22,36 @@ const addToCart = async (req, res) => {
 }
 
 // remove item from user cart
+/**
+ * Removes an item from the user's cart
+ * @async
+ * @function removeFromCart
+ * @param {Object} req - Express request object
+ * @param {Object} req.body - Request body
+ * @param {string} req.body.userId - The ID of the user whose cart item should be removed
+ * @param {string} req.body.itemId - The ID of the food/item to remove from cart
+ * @param {Object} res - Express response object
+ * @returns {Promise<void>}
+ * @throws {Error} If user not found or database operation fails
+ */
 const removeFromCart = async (req, res) => {
-
+    try {
+        let userData = await userModel.findById(req.body.userId);
+        let cartData = await userData.cartData;
+        if(cartData[req.body.itemId] > 0){
+            cartData[req.body.itemId] -= 1;
+        }
+        await userModel.findByIdAndUpdate(req.body.userId, {cartData});
+        res.json({success: true, message: "Removed from Cart"});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:"Error"})
+    }
 }
 
 // fetch user cart data
 const getCart = async (req, res) => {
-
+    
 }
 
 export {addToCart, removeFromCart, getCart}
