838374d75f6277eac55637294c8cd6434a095833
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-12-17 15:15:52 +0530

    added payment methods

--- front-food/src/pages/PlaceOrder/PlaceOrder.jsx
+++ front-food/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,33 +1,76 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./PlaceOrder.css";
 import { StoreContext } from "../../components/context/StoreContext";
 import "./../Cart/Cart.css"
+import axios from "axios";
 
 function PlaceOrder() {
 
-  const {getTotalCartAmount} = useContext(StoreContext);
+  const {getTotalCartAmount, url, token, food_list, cartItem} = useContext(StoreContext);
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    pincode: "",
+    country: "",
+    contact: ""
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data => ({...data, [name]:value}))
+  }
+
+  const placeOrder = async (event) => {
+    event.preventDefault();
+    let orderItems = []; // here we will add cart data
+    food_list.map((item)=> {
+      if(cartItem[item._id] > 0) {  // - Collect items from a shopping cart (cartItem) that have a quantity greater than 0.
+        let itemInfo = {... item};  // - Copies the item into itemInfo. {...item} makes deep copy
+        itemInfo["quantity"] = cartItem[item._id]; // - Adds a new property quantity to itemInfo, setting it to the number of that item in the cart.
+        orderItems.push(itemInfo); // - Pushes this updated itemInfo into the orderItems array.
+      }
+    })
+    let orderData = {
+      address: data,
+      items: orderItems,
+      amount: getTotalCartAmount()+2,
+    }
+    let response = await axios.post(url+"api/order/place", orderData, {headers:{token}});
+    if(response.data.success){
+      const {session_url} = response.data;  // fetching response.data from backend. as success_url
+      window.location.replace(session_url);  // keep it for visa 4242 4242 4242 4242, 09/21 -> as manufacturing, and cvc as ->123 
+    }
+    else {
+      alert("Error")
+    }
+  }
 
   return (
-    <form className="place-order">
+    <form onSubmit={placeOrder} className="place-order">
       
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
 
         <div className="multi-field">
-          <input type="text" placeholder="First name" required />
-          <input type="text" placeholder="Last name" />
+          <input name='firstName' onChange={onChangeHandler} value={data.firstName} type="text" placeholder="First name" required />
+          <input name='lastName' onChange={onChangeHandler} value={data.lastName} type="text" placeholder="Last name" />
         </div>
-        <input type="email" placeholder="Email address" required />
-        <input type="text" placeholder="Your Street" required />
+        <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder="Email address" required />
+        <input name='street' onChange={onChangeHandler} value={data.street} type="text" placeholder="Your Street" required />
         <div className="multi-field">
-          <input type="text" placeholder="Your City" required />
-          <input type="text" placeholder="Your State" required />
+          <input name='city' onChange={onChangeHandler} value={data.city} type="text" placeholder="Your City" required />
+          <input name='state' onChange={onChangeHandler} value={data.state} type="text" placeholder="Your State" required />
         </div>
         <div className="multi-field">
-          <input type="text" placeholder="Your Pin-code" required />
-          <input type="text" placeholder="Your Contact" required />
+          <input name='pincode' onChange={onChangeHandler} value={data.pincode} type="text" placeholder="Your Pin-code" required />
+          <input name='contact' onChange={onChangeHandler} value={data.contact}  type="text" placeholder="Your Contact" required />
         </div>
-        <input type="text" placeholder="Your Landmark " required />
+        <input  name='country' onChange={onChangeHandler} value={data.country} type="text" placeholder="Your Landmark " required />
 
       </div>
 
@@ -49,9 +92,10 @@ function PlaceOrder() {
               <b>${getTotalCartAmount()=== 0? 0 : Math.floor(getTotalCartAmount() + 2)}</b>
             </div>
           </div>
-          <button >PROCEED TO PAYMENT</button>
+          <button type="submit">PROCEED TO PAYMENT</button>
         </div>
       </div>
+      
     </form>
   )
 }
