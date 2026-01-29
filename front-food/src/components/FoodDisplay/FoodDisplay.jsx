12ca9618cf7360a3d7946d27f141f20f7be4b2d3
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2026-01-29 21:33:46 +0530

    added changes in Frontend

--- front-food/src/components/FoodDisplay/FoodDisplay.jsx
+++ front-food/src/components/FoodDisplay/FoodDisplay.jsx
@@ -13,10 +13,17 @@ function FoodDisplay({category}) {
       <div className="food-display-list">
         {
           // if category prop is provided and not 'All', filter by category
-          (category && category !== 'All' ? food_list.filter(f => f.category === category) : food_list)
+         /* (category && category !== 'All' ? food_list?.length > 0 && food_list.filter(f => f.category === category) : food_list)
             .map((item) => {
               return <FoodItem key={item._id} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}  />
-            })
+            })*/
+
+          food_list.length > 0 && food_list.filter((item) => category === 'All' || item.category === category)
+          .map((item) => (
+            <FoodItem key={item._id} id={item._id} name={item.name} description={item.description}
+              price={item.price} image={item.image}
+             />
+          ))
         }
       </div>
     </div>
