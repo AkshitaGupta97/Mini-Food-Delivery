5c0e8ab4fd4aa0e7697b733d7cd27cd834dea72a
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2026-02-01 15:59:35 +0530

    added changes

--- backend-food/config/db.js
+++ backend-food/config/db.js
@@ -1,30 +1,20 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+import 'dotenv/config';
 
-dotenv.config();
+export const connectDB = async () => {
+  try {
+    mongoose.set("bufferCommands", false);
 
-export const connectDB = async() => {
-    // Use MongoDB URI from environment variables or fallback to local MongoDB
-   // const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/food-delivery";
-    
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
 
-  /* try {
-    const mongoURI = "mongodb+srv://gungunakshita37gupta973_db_user:food123@cluster0.eqxxxzn.mongodb.net/food-delivery";
-    await mongoose.connect(mongoURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-   }
-    catch(error){
-        console.error("✗ MongoDB connection failed:", error.message);
-        process.exit(1); // Exit process if DB connection fails
-    }
-    */
+    console.log("✓ MongoDB connected successfully");
 
-  await mongoose.connect('mongodb+srv://gungunakshita37gupta973_db_user:food123@cluster0.eqxxxzn.mongodb.net/food-delivery')
-   .then(() => console.log('✓ MongoDB connected successfully'))
-   .catch((error) => console.error("✗ MongoDB connection failed:", error.message));
-    
-   
-}
+    console.log("DB State:", mongoose.connection.readyState);
 
+  } catch (error) {
+    console.error("✗ MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
